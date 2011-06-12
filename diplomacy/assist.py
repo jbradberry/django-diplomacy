22d78f2e12f832eb950564cf28d70354a3bd3f3d
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-06-12 11:56:03 -0400

    Added assist.py, with a helper for setting up DATC tests.

---
+++ diplomacy/assist.py
@@ -0,0 +1,46 @@
+from diplomacy import models
+from collections import defaultdict
+
+convert = {'L': 'A', 'S': 'F'}
+
+class Setup(object):
+    """
+    Helper class to make setting up DATC tests much easier.
+    
+    """
+
+    def __init__(self):
+        self.index = 1
+        self.u_index = 1
+        self.gvt_index = defaultdict(int)
+        self.gvt = dict((unicode(g.power), g)
+                        for g in models.Government.objects.all())
+        self.sr = dict(("%s %s" % (convert[sr.sr_type], unicode(sr)), sr)
+                       for sr in models.Subregion.objects.all())
+        self.sr[None] = None
+        models.Order.objects.all().delete()
+        models.Unit.objects.all().delete()
+        models.Turn.objects.exclude(number=0).delete()
+        self.turn = models.Turn.objects.get()
+
+    def order(self, gvt, actor, action, assist, target):
+        if action != 'B':
+            models.Unit.objects.create(id=self.u_index,
+                                       turn=self.turn,
+                                       government=self.gvt[gvt],
+                                       u_type=convert[self.sr[actor].sr_type],
+                                       subregion=self.sr[actor])
+            self.u_index += 1
+        if target:
+            sr = assist if assist else actor
+            target = "%s %s" % (convert[self.sr[sr].sr_type], target)
+        models.Order.objects.create(id=self.index,
+                                    turn=self.turn,
+                                    government=self.gvt[gvt],
+                                    slot=self.gvt_index[gvt],
+                                    actor=self.sr[actor],
+                                    action=action,
+                                    assist=self.sr[assist],
+                                    target=self.sr[target])
+        self.index += 1
+        self.gvt_index[gvt] += 1
