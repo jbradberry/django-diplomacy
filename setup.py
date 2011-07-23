5bc5a4a7322515aeb4272b6b20f185f92aa510a1
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-07-22 23:01:09 -0400

    Added code to setup.py to include data files with the distribution.

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 from distutils.core import setup
+import os
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('diplomacy'):
+    # Ignore dirnames that start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.')]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[10:] # Strip "diplomacy/" or "diplomacy\"
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
 
 setup(name='django-diplomacy',
       description='A play-by-web app for Diplomacy',
@@ -6,7 +25,9 @@ setup(name='django-diplomacy',
       author='Jeff Bradberry',
       author_email='jeff.bradberry@gmail.com',
       url='http://github.com/jbradberry/django-diplomacy',
-      packages=["diplomacy"],
+      package_dir={'diplomacy': 'diplomacy'},
+      packages=packages,
+      package_data={'diplomacy': data_files},
       classifiers=['Development Status :: 2 - Pre-Alpha',
                    'Environment :: Web Environment',
                    'Framework :: Django',
