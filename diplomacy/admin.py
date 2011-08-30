97722e5a51745caabe0623da63056736c0773d60
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-08-30 19:16:04 -0400

    Added Turns and Governments to the admin.

--- diplomacy/admin.py
+++ diplomacy/admin.py
@@ -1,10 +1,18 @@
-from diplomacy.models import Game, Territory, Subregion
+from diplomacy.models import Game, Turn, Government, Territory, Subregion
 from django.contrib import admin
 
 class GameAdmin(admin.ModelAdmin):
-    fields = ['name', 'slug', 'description', 'owner', 'state', 'open_joins']
+    fields = ('name', 'slug', 'description', 'owner', 'state', 'open_joins')
     list_display = ('name', 'owner', 'created', 'state', 'open_joins')
     prepopulated_fields = {"slug": ("name",)}
+
+class TurnAdmin(admin.ModelAdmin):
+    fields = ('game', 'number', 'year', 'season')
+    date_hierarchy = 'generated'
+    list_display = ('game', 'number', 'year', 'season')
+
+class GovernmentAdmin(admin.ModelAdmin):
+    list_display = ('game', 'user', 'name', 'power')
 
 class TerritoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'power', 'is_supply')
@@ -14,5 +22,7 @@ class SubregionAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'sr_type', 'init_unit')
 
 admin.site.register(Game, GameAdmin)
+admin.site.register(Turn, TurnAdmin)
+admin.site.register(Government, GovernmentAdmin)
 admin.site.register(Territory, TerritoryAdmin)
 admin.site.register(Subregion, SubregionAdmin)
