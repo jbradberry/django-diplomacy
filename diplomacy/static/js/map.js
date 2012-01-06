084f487533ecc0680f8e232f709338ca893ff41f
Jeff Bradberry <jeff.bradberry@gmail.com>
2012-01-05 23:58:32 -0500

    Added a title to each unit token on the map.

--- diplomacy/static/js/map.js
+++ diplomacy/static/js/map.js
@@ -16,13 +16,17 @@ function tokenizemap($mapsvg) {
   var $army = $($mapsvg.find("#army_prototype")[0]);
   var $fleet = $($mapsvg.find("#fleet_prototype")[0]);
   $.each(diplomacy_map.units, function(i, v) {
-    if ( v[1] === 'A' ) { var $unit = $army.clone(); }
-    else { var $unit = $fleet.clone(); }
+    var $unit;
+    if ( v[1] === 'A' ) { $unit = $army.clone(); }
+    else { $unit = $fleet.clone(); }
 
     var style = "color:#000000;fill:" + diplomacy_map.colors[v[2]] + ";stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:none;stroke-dashoffset:0;marker:none;visibility:visible;display:inline;overflow:visible;enable-background:accumulate;opacity:1";
     $unit.attr({'id': v[1] + i, 'style': style,
-		'x': diplomacy_map.coordinates[v[0]][0],
-		'y': diplomacy_map.coordinates[v[0]][1]});
+                'x': diplomacy_map.coordinates[v[0]][0],
+                'y': diplomacy_map.coordinates[v[0]][1],
+                'title': v[1] + " " + v[0]
+               });
+    $unit.find("title").text(v[1]+" "+v[0]);
     $tokens.append($unit);
     diplomacy_map.total_units += 1;
   });
