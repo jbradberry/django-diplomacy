31265e412c8c6c5ec9aaea7108dfd5544e32302c
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-05-24 11:06:15 -0400

    Converted views to class-based views.

--- diplomacy/urls.py
+++ diplomacy/urls.py
@@ -1,17 +1,23 @@
 from django.conf.urls.defaults import patterns, url, include
 from django.conf import settings
 
+from . import views
 
-urlpatterns = patterns('diplomacy.views',
-    url(r'^games/$', 'game_list', name='diplomacy_game_list'),
-    url(r'^games/(?P<slug>[-\w]+)/$', 'game_detail', name='diplomacy_game_detail'),
-    url(r'^games/(?P<slug>[-\w]+)/master/$', 'game_master', name='diplomacy_game_master'),
-    url(r'^games/(?P<slug>[-\w]+)/map/$', 'map_view', name='diplomacy_game_map'),
+
+urlpatterns = patterns('',
+    url(r'^games/$', views.GameListView.as_view(), name='diplomacy_game_list'),
+    url(r'^games/(?P<slug>[-\w]+)/$', views.GameDetailView.as_view(),
+        name='diplomacy_game_detail'),
+    url(r'^games/(?P<slug>[-\w]+)/master/$', views.GameMasterView.as_view(),
+        name='diplomacy_game_master'),
+    url(r'^games/(?P<slug>[-\w]+)/map/$', views.MapView.as_view(),
+        name='diplomacy_game_map'),
     url(r'^games/(?P<slug>[-\w]+)/turn/(?P<season>[A-Z]+)(?P<year>\d+)/$',
-        'game_detail', name='diplomacy_turn_detail'),
+        views.GameDetailView.as_view(), name='diplomacy_turn_detail'),
     url(r'^games/(?P<slug>[-\w]+)/turn/(?P<season>[A-Z]+)(?P<year>\d+)/map/$',
-        'map_view', name='diplomacy_turn_map'),
-    url(r'^games/(?P<slug>[-\w]+)/orders/(?P<power>[-\w]+)/$', 'orders', name='diplomacy_orders'),
+        views.MapView.as_view(), name='diplomacy_turn_map'),
+    url(r'^games/(?P<gameslug>[-\w]+)/orders/(?P<slug>[-\w]+)/$',
+        views.OrdersView.as_view(), name='diplomacy_orders'),
 )
 
 if 'micropress' in settings.INSTALLED_APPS:
--- diplomacy/views.py
+++ diplomacy/views.py
@@ -1,112 +1,193 @@
-from django.views.generic.list_detail import object_list, object_detail
-from django.views.generic.simple import direct_to_template
-from django.shortcuts import get_object_or_404, redirect
-from django.forms.formsets import formset_factory
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import BaseFormView
+from django.core.urlresolvers import reverse
+from django.core.exceptions import PermissionDenied
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.http import (HttpResponse, HttpResponseRedirect,
-                         HttpResponseForbidden)
+from django.shortcuts import get_object_or_404
+from django.forms.formsets import formset_factory
 from django.contrib import messages
-from django.db.models import ForeignKey, Max
+from django.db.models import Max
+
 import json
-from .models import (Game, Government, Turn, Order, Territory,
-                     Subregion, DiplomacyPrefs)
-from .forms import OrderForm, OrderFormSet, GameMasterForm
+
+from . import models, forms
 
 
-def game_list(request, page=None, paginate_by=30, state=None, **kwargs):
-    game_list = Game.objects.annotate(t=Max('turn__generated')).order_by('-t')
+class GameListView(ListView):
+    queryset = models.Game.objects.annotate(
+        t=Max('turn__generated')).order_by('-t')
 
-    if state is None:
-        state = request.GET.get('state', None)
 
-    if state is not None:
-        game_list = game_list.filter(state__iexact=state)
-    return object_list(request,
-                       queryset=game_list,
-                       paginate_by=paginate_by,
-                       page=page,
-                       template_object_name="game",
-                       extra_context={"state": state},
-                       **kwargs)
+class GameDetailView(DetailView):
+    model = models.Game
 
-def game_detail(request, slug, season=None, year=None):
-    game = get_object_or_404(Game, slug=slug)
-    if season is None and year is None:
-        t = game.current_turn()
-        current = t is not None
-        setup = not current
-    else:
-        t = get_object_or_404(game.turn_set, season=season, year=year)
-        current, setup = False, False
-    context = {'game': game, 'turn': t, 'current': current,
-               'governments': game.governments(t), 'setup': setup}
-    return direct_to_template(request, 'diplomacy/game_detail.html',
-                              extra_context=context)
+    def get_context_data(self, **kwargs):
+        season, year = self.kwargs.get('season'), self.kwargs.get('year')
+        if season is None and year is None:
+            t = self.object.current_turn()
+            current = t is not None
+            setup = not current
+        else:
+            t = get_object_or_404(
+                self.object.turn_set, season=season, year=year)
+            current, setup = False, False
 
-@login_required
-def game_master(request, slug):
-    game = get_object_or_404(Game, slug=slug)
-    actors = sum(g.actors().count() for g in game.government_set.all())
-    if request.user != game.owner:
-        return HttpResponseForbidden("<h1>Permission denied</h1>")
-    form = GameMasterForm(request.POST or None)
-    if form.is_valid():
-        if request.POST.get('activate', False) and game.state == 'S':
-            game.activate()
-        if request.POST.get('generate', False) and game.state == 'A':
-            game.generate()
-        if request.POST.get('pause', False) and game.state == 'A':
-            game.state = 'P'
-            game.save()
-        if request.POST.get('close', False) and game.state == 'A':
-            game.state = 'F'
-            game.save()
-        if request.POST.get('unpause', False) and game.state == 'P':
-            game.state = 'A'
-            game.save()
-        return redirect('diplomacy_game_master', slug=slug)
-    return direct_to_template(request, 'diplomacy/game_master.html',
-                              extra_context={'game': game, 'form': form,
-                                             'actors': actors})
+        context = {'turn': t, 'current': current, 'setup': setup,
+                   'governments': self.object.governments(t)}
+        context.update(**kwargs)
+        return super(GameDetailView, self).get_context_data(**context)
 
-@login_required
-def orders(request, slug, power):
-    g = Game.objects.get(slug=slug)
-    try:
-        gvt = g.government_set.get(power__name__iexact=power,
-                                   user=request.user)
-    except Government.DoesNotExist:
-        return HttpResponseForbidden("<h1>Permission denied</h1>")
 
-    OFormSet = formset_factory(form=OrderForm, formset=OrderFormSet, extra=0)
+class GameMasterView(DetailView, BaseFormView):
+    model = models.Game
+    form_class = forms.GameMasterForm
+    template_name = 'diplomacy/game_master.html'
 
-    turn = g.current_turn()
-    order = gvt.order_set.filter(turn=turn)
-    formset = OFormSet(gvt, not order.exists(), request.POST or None,
-                       initial=turn.normalize_orders(gvt))
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(GameMasterView, self).dispatch(*args, **kwargs)
 
-    if formset.is_valid():
-        messages.success(request, "Your orders have been submitted.",
+    def get(self, request, **kwargs):
+        self.object = self.get_object()
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        return self.render_to_response(
+            self.get_context_data(object=self.object,
+                                  form=form)
+        )
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super(GameMasterView, self).post(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        obj = super(GameMasterView, self).get_object(queryset)
+        if self.request.user != obj.owner:
+            raise PermissionDenied
+        return obj
+
+    def get_context_data(self, **kwargs):
+        context = {
+            'actors': sum(
+                g.actors().count() for g in self.object.government_set.all())
+        }
+        context.update(**kwargs)
+        return super(GameMasterView, self).get_context_data(**context)
+
+    def form_valid(self, form):
+        # FIXME: We should validate the input plus state.
+        if self.request.POST.get('activate') and self.object.state == 'S':
+            self.object.activate()
+        if self.request.POST.get('generate') and self.object.state == 'A':
+            self.object.generate()
+        if self.request.POST.get('pause') and self.object.state == 'A':
+            self.object.state = 'P'
+            self.object.save()
+        if self.request.POST.get('close') and self.object.state == 'A':
+            self.object.state = 'F'
+            self.object.save()
+        if self.request.POST.get('unpause') and self.object.state == 'P':
+            self.object.state = 'A'
+            self.object.save()
+        return super(GameMasterView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('diplomacy_game_master',
+                       kwargs={'slug': self.object.slug})
+
+
+class OrdersView(DetailView, BaseFormView):
+    model = models.Government
+    slug_field = 'power__name__iexact'
+
+    form_class = formset_factory(form=forms.OrderForm,
+                                 formset=forms.OrderFormSet,
+                                 extra=0)
+
+    template_name = 'diplomacy/manage_orders.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(OrdersView, self).dispatch(*args, **kwargs)
+
+    def get(self, request, **kwargs):
+        self.object = self.get_object()
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        return self.render_to_response(
+            self.get_context_data(object=self.object,
+                                  form=form)
+        )
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super(OrdersView, self).post(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return self.model.objects.filter(
+            game__slug=self.kwargs.get('gameslug', ''))
+
+    def get_object(self, queryset=None):
+        obj = super(OrdersView, self).get_object(queryset)
+
+        if self.request.user != obj.user:
+            raise PermissionDenied
+        return obj
+
+    def get_context_data(self, **kwargs):
+        order_filter = {
+            'unit_fixed': (self.object.game.current_turn().season != 'FA'),
+            'tree': self.object.filter_orders()
+        }
+        context = {
+            'game': self.object.game,
+            'current': True,
+            'order_filter': json.dumps(order_filter),
+        }
+        context.update(**kwargs)
+        return super(OrdersView, self).get_context_data(**context)
+
+    def get_initial(self):
+        return self.object.game.current_turn().normalize_orders(self.object)
+
+    def get_form_kwargs(self):
+        kwargs = super(OrdersView, self).get_form_kwargs()
+        turn = self.object.game.current_turn()
+        kwargs.update(
+            government=self.object,
+            first_submit=not self.object.order_set.filter(turn=turn).exists()
+        )
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, "Your orders have been submitted.",
                          fail_silently=True)
-        prefs = DiplomacyPrefs.objects.filter(user=request.user)
+        prefs = models.DiplomacyPrefs.objects.filter(user=self.request.user)
         if prefs and prefs.get().warnings:
-            for w in formset.irrational_orders():
-                messages.warning(request, w, fail_silently=True)
-        formset.save()
-        return HttpResponseRedirect('')
+            for w in form.irrational_orders():
+                messages.warning(self.request, w, fail_silently=True)
+        return super(OrdersView, self).form_valid(form)
 
-    order_filter = {'unit_fixed': (g.current_turn().season != 'FA'),
-                    'tree': gvt.filter_orders()}
-    context = {'formset': formset, 'game': g, 'current': True,
-               'order_filter': json.dumps(order_filter)}
-    return direct_to_template(request, 'diplomacy/manage_orders.html',
-                              extra_context=context)
+    def get_success_url(self):
+        return reverse('diplomacy_orders',
+                       kwargs={'gameslug': self.object.game.slug,
+                               'slug': self.object.power.name})
 
-def map_view(request, slug, season=None, year=None):
-    game = get_object_or_404(Game, slug=slug)
-    if year:
-        t = get_object_or_404(Turn, game=g, season=season, year=year)
-    else:
-        t = game.current_turn()
-    context = {'game': game, 'turn': t}
-    return direct_to_template(request, 'diplomacy/map.html', context)
+
+class MapView(DetailView):
+    model = models.Game
+    template_name = 'diplomacy/map.html'
+
+    def get_context_data(self, **kwargs):
+        season, year = self.kwargs.get('season'), self.kwargs.get('year')
+        if season is None and year is None:
+            t = self.object.current_turn()
+        else:
+            t = get_object_or_404(
+                self.object.turn_set, season=season, year=year)
+
+        context = {'turn': t}
+        context.update(**kwargs)
+        return super(MapView, self).get_context_data(**context)
